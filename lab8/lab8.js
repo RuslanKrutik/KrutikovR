fe2a11029af80575160ad4d30789a05af12beb60
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-05-25 19:06:36 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -2,6 +2,7 @@ function showDate() {
     let out = document.getElementById('current-date');
     let localesOut = document.getElementById('locale-dates');
     let today = new Date();
+    let additionalInfo = document.getElementById('additional-info');
 
     out.innerHTML = "Текущая дата и время:";
     out.style.color = "white"
@@ -21,4 +22,11 @@ function showDate() {
         let localeDate = today.toLocaleString(item.locale);
         localesOut.innerHTML += `<div class="locale-date"><span>${item.country} (${item.locale}):</span> ${localeDate}</div>`;
     });
+
+    additionalInfo.innerHTML = `
+        <div class="additional-info-item"><span>Текущий год:</span> ${today.getFullYear()}</div>
+        <div class="additional-info-item"><span>Текущий месяц:</span> ${today.toLocaleString('ru-RU', { month: 'long' })}</div>
+        <div class="additional-info-item"><span>Текущая дата:</span> ${today.getDate()}</div>
+        <div class="additional-info-item"><span>День недели:</span> ${today.toLocaleString('ru-RU', { weekday: 'long' })}</div>
+    `;
 }
