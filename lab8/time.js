1aaa21fb680f090ae1ed67b8bce856b47f500f21
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-05-25 20:27:22 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -5,6 +5,10 @@ function showTime(){
 
     let seconds = today.getSeconds();
     document.getElementById('seconds').innerHTML = seconds < 10 ? '0' + seconds : seconds;
+
+    let secondHand = document.getElementById('second-hand');
+    let degree = seconds * 6;
+    secondHand.style.transform = `rotate(${degree}deg)`;
 }
 
 setInterval(showTime, 1000);
